993ef2659b2c4620d6d2b8ca010fd89db66fd321
Midhun <71970176+midhun-babu@users.noreply.github.com>
2026-05-05 17:50:57 +0530

    Update useGps.js

--- frontend/src/hooks/useGps.js
+++ frontend/src/hooks/useGps.js
@@ -1,5 +1,4 @@
 import { useState, useRef, useCallback } from 'react';
-
 export const useGps = (addAlert) => {
     const [userLocation, setUserLocation] = useState(null);
     const [gpsLoading, setGpsLoading] = useState(false);
